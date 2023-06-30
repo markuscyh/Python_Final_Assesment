334cc8908a4e716e4f5194b2a783eaf6efadf53e
markuscyh <yhchan1902@gmail.com>
2023-06-30 09:30:39 +0800

    Updated source file
    added a new option called "Display Reservations" in the main menu

--- python_21100649.py
+++ python_21100649.py
@@ -6,6 +6,12 @@ import os
 
 
 
+# This function generates a meal recommendation
+def meal_recommendation():
+    menu_list = open(menuItems_Student)
+    print = random.choice()
+    pass
+
 # From this point below is the code that will first be executed when running the program
 main_menu_loop = True
 while main_menu_loop == True:
@@ -14,14 +20,14 @@ while main_menu_loop == True:
     # This loop contains exception handling to help ensure that the proper input is entered 
     loop = True
     while loop == True:
-        print("[1] Add Reservation \n[2] Cancel Reservation \n[3] Edit Reservations \n[4] Generate Meal Recommendation \n[5] Exit")
+        print("[1] Add Reservation \n[2] Cancel Reservation \n[3] Edit Reservations \n[4] Display Reservations \n[5] Generate Meal Recommendation \n[6] Exit")
 
         try:
             main_menu_input = int(input())
         except:
             print("Please enter a valid input. \n")
         else:
-            if main_menu_input < 1 or main_menu_input > 5:
+            if main_menu_input < 1 or main_menu_input > 6:
                 print("Please type the number that corresponds to the option you would like to select. \n")
             else:
                 loop = False
@@ -39,6 +45,8 @@ while main_menu_loop == True:
         pass
         #function
     elif main_menu_input == 5:
+        meal_recommendation()
+    elif main_menu_input == 6:
         main_menu_loop = False
 
 print("\nThank you for using the program")
