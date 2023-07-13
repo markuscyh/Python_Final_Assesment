7edba01c336c3c4fe3a2f0f90b5321bf82d40a9c
markuscyh <yhchan1902@gmail.com>
2023-07-13 14:02:10 +0800

    Input update and "Display" UI update
    made it more clear where the user input will occur along with making it neater

--- python_21100649.py
+++ python_21100649.py
@@ -33,11 +33,13 @@ def main_menu_exit(question):
 # This block of code will create the Reservation Display list for the Display function of the program
 reservation_list = open("reservation_21100649.txt", "r")
 reservation_list_contents = reservation_list.readlines()
-display_list = tuple(reservation_list_contents)
+display_list = reservation_list_contents
+display_list.sort()
+display_list = tuple(display_list)
 
 main_menu_loop = True
 while main_menu_loop:
-    print("\nWelcome to Charming Thyme Trattoria. Please type the number that corresponds to the option you would like to select")
+    print("\nWelcome to Charming Thyme Trattoria. Please enter the number that corresponds to the option you would like to select")
 
     # This loop contains exception handling to help ensure that the proper input is entered 
     loop = True
@@ -45,7 +47,7 @@ while main_menu_loop:
         print("[1] Add Reservation \n[2] Cancel Reservation \n[3] Edit Reservations \n[4] Display Reservations \n[5] Generate Meal Recommendation \n[6] Exit")
 
         try:
-            main_menu_input = int(input())
+            main_menu_input = int(input(">>> "))
         except:
             print("Please enter a valid input. \n")
             print("--------------------------------------------------------------------------------\n")
@@ -68,11 +70,15 @@ while main_menu_loop:
 
     elif main_menu_input == 4:
         print("\n--------------------------------------------------------------------------------\n")
+        display_guide = "No. | {0:^10} | {1:^6} | {2:^15} | {3:^31} | {4:^10} | {5:^1} \n".format("Date","Slot","Name","Email","Phone Num","PAX")
+        print(display_guide)
+        list_count = 1
         for i in range(len(display_list)):
             display_list_contents = display_list[i]
             display_list_contents = display_list_contents.split("|")
-            display_output = "{0:<1} | {1:<1} | {2:<15} | {3:<31} | {4:<10} | {5:<1}".format(display_list_contents[0], display_list_contents[1], display_list_contents[2], display_list_contents[3], display_list_contents[4], display_list_contents[5])
+            display_output = "{0:^3} | {1:^1} | {2:^1} | {3:^15} | {4:^31} | {5:^10} |  {6:^1}".format(list_count,display_list_contents[0], display_list_contents[1], display_list_contents[2], display_list_contents[3], display_list_contents[4], display_list_contents[5])
             print(display_output)
+            list_count += 1
         # This input will allow the user to view the display list before returning to the main menu
         display_exit = input("Enter any word, number or symbol to return to the main menu: ")
         os.system('cls')
@@ -85,7 +91,7 @@ while main_menu_loop:
         loop = True
         while loop:
             print(f"We recommend {meal_recommendation_generator()}")
-            loop_question = "Would you like a new recommendation or return to the main menu? \n[1] Another recommendation \n[2] Exit \n"
+            loop_question = "Would you like a new recommendation or return to the main menu? \n[1] Another recommendation \n[2] Exit \n>>> "
             loop = main_menu_exit(loop_question)
         os.system('cls')
         print("\n--------------------------------------------------------------------------------")
