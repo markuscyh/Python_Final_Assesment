cc6b71f4947cbac0146e2faa999d90e851518866
markuscyh <yhchan1902@gmail.com>
2023-07-13 13:37:33 +0800

    Update 13/7
    Fixed a bug with menu recommendations and improved the "Display" function

--- python_21100649.py
+++ python_21100649.py
@@ -33,7 +33,7 @@ def main_menu_exit(question):
 # This block of code will create the Reservation Display list for the Display function of the program
 reservation_list = open("reservation_21100649.txt", "r")
 reservation_list_contents = reservation_list.readlines()
-reservation_list.close()
+display_list = tuple(reservation_list_contents)
 
 main_menu_loop = True
 while main_menu_loop:
@@ -52,7 +52,7 @@ while main_menu_loop:
         else:
             if main_menu_input < 1 or main_menu_input > 6:
                 print("Please type the number that corresponds to the option you would like to select. \n")
-                print("----------------------------------------------------------------------------\n")
+                print("--------------------------------------------------------------------------------\n")
             else:
                 loop = False
     
@@ -68,14 +68,15 @@ while main_menu_loop:
 
     elif main_menu_input == 4:
         print("\n--------------------------------------------------------------------------------\n")
-        list_count = 0
-        for i in range(len(reservation_list_contents)):
-            print(reservation_list_contents[list_count])
-            list_count += 1
+        for i in range(len(display_list)):
+            display_list_contents = display_list[i]
+            display_list_contents = display_list_contents.split("|")
+            display_output = "{0:<1} | {1:<1} | {2:<15} | {3:<31} | {4:<10} | {5:<1}".format(display_list_contents[0], display_list_contents[1], display_list_contents[2], display_list_contents[3], display_list_contents[4], display_list_contents[5])
+            print(display_output)
         # This input will allow the user to view the display list before returning to the main menu
         display_exit = input("Enter any word, number or symbol to return to the main menu: ")
         os.system('cls')
-        print("\n--------------------------------------------------------------------------------\n")
+        print("\n--------------------------------------------------------------------------------")
 
         
     elif main_menu_input == 5:
@@ -87,12 +88,13 @@ while main_menu_loop:
             loop_question = "Would you like a new recommendation or return to the main menu? \n[1] Another recommendation \n[2] Exit \n"
             loop = main_menu_exit(loop_question)
         os.system('cls')
-        print("\n--------------------------------------------------------------------------------\n")
+        print("\n--------------------------------------------------------------------------------")
 
 
     elif main_menu_input == 6:
         main_menu_loop = False
 
+reservation_list.close()
 print("--------------------------------------------------------------------------------")
 print("Thank you for using the program")
 
