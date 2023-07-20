bbbd48ffdabd32fd0d442384fdefdd3541971b86
melody2002 <137771195+melody2002@users.noreply.github.com>
2023-07-20 12:50:05 +0800

    Update update reservation.py

--- update reservation.py
+++ update reservation.py
@@ -1,4 +1,4 @@
-ffrom datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
 
 # Function to update the reservation
@@ -97,7 +97,7 @@ while main_menu_loop :
 
                 valid_choices = ['1', '2', '3', '4', '5', '6']
                 user_update = input("Please choose a number (1-6) or 'N' to stop updating this reservation: ")
-
+                # Code for new date
                 if user_update in valid_choices :
                     if user_update == '1' :
                         while True :
@@ -111,7 +111,7 @@ while main_menu_loop :
                                     print("Invalid Date! Reservation Should Be Made At Least 5 Days in Advance.")
                                     continue
 
-                                # Check if the chosen slot on the chosen date has reached the maximum capacity (8 reservations)
+                    # Check if the chosen slot on the chosen date has reached the maximum capacity (8 reservations)
                                 date_slot = (date, reservation_info[1])
                                 date_slot_count = count_reservations()
                                 if date_slot in date_slot_count and date_slot_count[date_slot] >= 8 :
@@ -123,6 +123,7 @@ while main_menu_loop :
                                 break
                             except ValueError :
                                 print("Invalid Date Format! Please Enter The Date in The Format yyyy-mm-dd.")
+                    # Code for new slot
                     elif user_update == '2' :
                         while True :
                             slot = input("Enter New Slot from 1-4: ")
@@ -140,6 +141,7 @@ while main_menu_loop :
                                 break
                             else :
                                 print("Invalid Slot! Please Choose Between 1-4.")
+                    # Code for new name
                     elif user_update == '3' :
                         while True :
                             name_new = input("Enter New Name: ").strip().upper()
@@ -149,6 +151,7 @@ while main_menu_loop :
                                 break
                             else :
                                 print("Name cannot be blank! Please enter a valid name.")
+                    # Code for new email
                     elif user_update == '4' :
                         while True :
                             email = input("Enter New Email: ").lower()
@@ -159,6 +162,7 @@ while main_menu_loop :
                                 break
                             else :
                                 print("Invalid Email! Only gmail, yahoo, and hotmail domains are accepted.")
+                    # Code for new number
                     elif user_update == '5' :
                         while True :
                             contact = input("Enter New Contact Number: ")
@@ -168,6 +172,7 @@ while main_menu_loop :
                                 break
                             else :
                                 print("Invalid Number! Contact Should Be a 10-Digit Number.")
+                    # Code for new number of people
                     elif user_update == '6' :
                         while True :
                             num_people = input("Enter New Number of People: ")
@@ -197,5 +202,6 @@ while main_menu_loop :
 
 print("--------------------------------------------------------------------------------")
 print("Thank you for using the program")
+
 
 
