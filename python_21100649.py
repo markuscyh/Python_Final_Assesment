299f9c4a915c574fe69318d265a0778588157df6
markuscyh <yhchan1902@gmail.com>
2023-07-22 22:39:16 +0800

    updated ui

--- python_21100649.py
+++ python_21100649.py
@@ -124,6 +124,7 @@ while main_menu_loop:
                 data = f.readlines()
             matching_reservations = []
 
+            os.system('cls')
             for i in range(len(data)) :
                 reservation_data = data[i].strip().split('|')
                 if len(reservation_data) >= 3 and reservation_data[2].upper() == reservation_name :
@@ -142,15 +143,16 @@ while main_menu_loop:
             while True :
                 try :
                     choice = int(input("Enter the number of the reservation you want to update: "))
-                    if 1 <= choice <= len(matching_reservations) :
+                    if 1 <= choice <= len(matching_reservations):
                         break
-                    else :
-                        print("Invalid choice! Please enter a valid number.")
-                except ValueError :
-                    print("Invalid input! Please enter a number.")
+                    else:
+                        print("\nInvalid choice! Please enter a valid number.")
+                except ValueError:
+                    print("\nInvalid input! Please enter a number.")
 
             selected_reservation_index = matching_reservations[choice - 1]
             reservation_info = data[selected_reservation_index].strip().split('|')
+            os.system('cls')
 
             while True :
                 # Ask the user for the field they want to update
@@ -169,6 +171,7 @@ while main_menu_loop:
                                 today = datetime.now().date()
                                 min_reservation_date = today + timedelta(days=5)  # Minimum reservation date 5 days in advance
                                 if chosen_date < min_reservation_date :
+                                    os.system('cls')
                                     print("Invalid Date! Reservation Should Be Made At Least 5 Days in Advance.")
                                     continue
                         # Check if the chosen slot on the chosen date has reached the maximum capacity (8 reservations)
@@ -181,76 +184,88 @@ while main_menu_loop:
                                     continue
 
                                 update_reservation(selected_reservation_index, 0, date)
+                                os.system('cls')
                                 print("Date updated successfully.")
                                 break
                             except ValueError :
                                 print("Invalid Date Format! Please Enter The Date in The Format yyyy-mm-dd.")
                     # Code for new slot
-                    elif user_update == '2' :
+                    elif user_update == '2':
                         while True :
                             slot = input("\nEnter New Slot from 1-4: ")
-                            if slot in ['1', '2', '3', '4'] :
+                            if slot in ['1', '2', '3', '4']:
                                 # Check if the chosen date and slot have reached the maximum capacity (8 reservations)
                                 date_slot = (reservation_info[0], 'Slot ' + slot)
                                 date_slot_count = count_reservations()
-                                if date_slot in date_slot_count and date_slot_count[date_slot] >= 8 :
+                                if date_slot in date_slot_count and date_slot_count[date_slot] >= 8:
                                     print("This slot on this date has reached the maximum capacity.")
                                     continue
 
                                 slot = 'Slot ' + slot
                                 update_reservation(selected_reservation_index, 1, slot)
+                                os.system('cls')
                                 print("Slot updated successfully.")
                                 break
-                            else :
+                            else:
+                                os.system('cls')
                                 print("Invalid Slot! Please Choose Between 1-4.")
                     # Code for new name
-                    elif user_update == '3' :
-                        while True :
+                    elif user_update == '3':
+                        while True:
                             name_new = input("\nEnter New Name: ").strip().upper()
-                            if name_new :
+                            if name_new:
                                 update_reservation(selected_reservation_index, 2, name_new)
+                                os.system('cls')
                                 print("Name updated successfully.")
                                 break
-                            else :
+                            else:
+                                os.system('cls')
                                 print("Name cannot be blank! Please enter a valid name.")
                     # Code for new email
                     
-                    elif user_update == '4' :
-                        while True :
+                    elif user_update == '4':
+                        while True:
                             email = input("\nEnter New Email: ")
                             if " " in email:
                                 print("Invalid Email! No spaces are allowed in an email.")
                             elif email.endswith('@gmail.com') or email.endswith('@yahoo.com') or email.endswith('@hotmail.com'):
                                 update_reservation(selected_reservation_index, 3, email)
+                                os.system('cls')
                                 print("Email updated successfully.")
                                 break
-                            else :
+                            else:
+                                os.system('cls')
                                 print("Invalid Email! Only gmail, yahoo, and hotmail domains are accepted.")
                     # Code for new number
-                    elif user_update == '5' :
-                        while True :
+                    elif user_update == '5':
+                        while True:
                             contact = input("\nEnter New Contact Number: ")
-                            if len(contact) == 10 and contact.isdigit() :
+                            if len(contact) == 10 and contact.isdigit():
                                 update_reservation(selected_reservation_index, 4, contact)
+                                os.system('cls')
                                 print("Contact Number updated successfully.")
                                 break
-                            else :
+                            else:
+                                os.system('cls')
                                 print("Invalid Number! Contact Should Be a 10-Digit Number.")
                     # Code for new number of people
-                    elif user_update == '6' :
-                        while True :
+                    elif user_update == '6':
+                        while True:
                             num_people = input("\nEnter New Number of People: ")
-                            if num_people in ['1', '2', '3', '4'] :
+                            if num_people in ['1', '2', '3', '4']:
                                 update_reservation(selected_reservation_index, 5, num_people)
+                                os.system('cls')
                                 print("Number of People updated successfully.")
                                 break
-                            else :
+                            else:
+                                os.system('cls')
                                 print("Invalid Number of People! Maximum is 4.")
-                elif user_update.upper() == 'N' :
+                elif user_update.upper() == 'N':
                     break
-
+            
+            os.system('cls')
             any_more_reservations = input("\nDo You Have Another Reservation To Update? (Y/N): ").upper()
-            if any_more_reservations != 'Y' :
+            if any_more_reservations != 'Y':
                 os.system('cls')
                 break
         
