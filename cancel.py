c2a63aa2a6101a407c03f43b69179ae16a3213cb
Jay202iqiwwiw <136432389+Jay202iqiwwiw@users.noreply.github.com>
2023-07-26 20:01:56 +0800

    Update cancel.py

--- cancel.py
+++ cancel.py
@@ -1,28 +1,23 @@
 # Function to cancel reservations
 def cancel_reservation():
+    print_reservations()
+    
     reservations = []
-
+        
     data = ''
-
-    with open('reservation_21100649.txt', 'r') as f:
-        data = f.readlines()
-
-    for i in range(len(data)):
+    
+    with open('reservation_21100649.txt', 'r') as f :
+            data = f.readlines()
+    
+    for i in range(len(data)) :
         reservations.append(data[i])
-
+    
     if not reservations:
-        print("No reservations available.")
         return
     else:
-
-        print("Reservations:")
-        for i, reservation in enumerate(reservations, 1):
-            data = reservation.split('|')
-            print("{} : Date - [{}] || Session - [{}] || Name - [{}]".format(i, data[0], data[1], data[2]))
-
         while True:
             try:
-                cancel_choice = int(input("Enter the number of the reservation you want to cancel (or 0 to go back): "))
+                cancel_choice = int(input("\nEnter the number of the reservation you want to cancel (or 0 to go back): "))
                 if cancel_choice == 0:
                     break
                 elif cancel_choice < 1 or cancel_choice > len(reservations):
@@ -30,11 +25,12 @@ def cancel_reservation():
                 else:
                     canceled_reservation = reservations.pop(cancel_choice - 1)
                     data = canceled_reservation.split('|')
-                    print("Reservation for {} on {} at {} has been canceled.".format(data[2], data[0], data[1]))
-
-                    with open('reservation_21100649.txt', 'w') as f:
-                        for line in reservations:
+                    print("Reservation for {} on {} at {} has been canceled.\n\n".format(data[2], data[0], data[1]))
+                    print_reservations()
+                    
+                    with open('reservation_21100649.txt', 'w') as f :
+                        for line in reservations :
                             f.write(line)
-                    break
+                            
             except ValueError:
                 print("Invalid input. Please enter a valid reservation number.")
